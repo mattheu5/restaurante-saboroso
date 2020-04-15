dc2718184a8f7d7f010f9d1a3e89eb5966f886a1
matheusmartins <matheusgleiciel@hotmail.com>
2020-04-15 17:54:03 -0300

    commit

---
+++ includes/db.js
@@ -0,0 +1,9 @@
+const mysql = require('mysql2');
+ 
+const connection = mysql.createConnection({
+  host: 'localhost',
+  user: 'root',
+  database: 'saboroso',
+  password: 'root'
+});
+module.exports = connection
---
+++ routes/users.js
@@ -0,0 +1,20 @@
+var express = require('express');
+var conn = require('../includes/db')
+var router = express.Router();
+
+/* GET users listing. */
+router.get('/', function(req, res, next) {
+  
+  conn.query('SELECT * FROM tb_users ORDER BY name', (err, results)=>{
+
+    if (err){
+      res.send(err)
+    } else {
+      res.send(results)
+    }
+
+  });
+
+});
+
+module.exports = router;
