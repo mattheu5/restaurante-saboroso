126a6483a20f1e299e4ae046cab75aa042a617a8
matheusmartins <matheusgleiciel@hotmail.com>
2020-04-18 17:09:00 -0300

    commit

--- public/db/original.sql
+++
@@ -1,5 +0,0 @@
-SELECT
-    (SELECT COUNT(*) FROM tb_contacts) AS nrcontacts,
-    (SELECT COUNT(*) FROM tb_menus) AS nrmenus,
-    (SELECT COUNT(*) FROM tb_reservations) AS nrreservations,
-    (SELECT COUNT(*) FROM tb_users) AS nrusers;
